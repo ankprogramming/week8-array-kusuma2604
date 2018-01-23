8a14175cf62a656e1014f88a10d050d30607dafb
Unknown <oppof1szaa168@gmail.com>
2018-01-23 11:17:46 +0700

    แก้ไขแล้ว ข้อ8.2

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,14 +3,31 @@
 using namespace std;
 
 int main () {
+   //     char city[] = {'A' , 'B', 'C', '\0'};
+    //   char city2[2] []= {"ABC","CDE"};
 
-        char name[] = {"Anukul"};
+
+        char name[] = {"kusuma"};
         int nameLength = 6;
-        cout << name;
+        cout << name << endl;
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
 
+        char revName[nameLength+1];
+        for (int i = 0 ; i < nameLength; i++){
+            revName [i] = name[5-i];
+        }
+
+
+       revName[nameLength] = '\0';
+       cout << revName  << endl;
+
+
+        for (int i = 0; i <6; i++) {
+            revName [i] = revName [i] + 1;
+        }
+        cout << revName ;
 
 
         return 0;
